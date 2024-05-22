7a2fc5473d147982f54b0794895c0fd59ac838bc
JoshdfG <agboolajoshua161@gmail.com>
2024-05-22 22:54:28 +0100

    chore: project setup

--- frontend/src/ui-components/Section/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./section.module.css";
-
-const Section = ({ children }) => {
-  return <section className={styles["section-container"]}>{children}</section>;
-};
-
-export default Section;
--- frontend/utils/contractAddress.js
+++
@@ -1,6 +0,0 @@
-export const FacoryAddr = () => {
-  return "0xA57E7a74AD5614C77118444324B9816002cE42e1";
-};
-export const ChildAddr = () => {
-  return "0x9F5cBaFDf6F5a7c03B0eDEb94558bb82e487157D";
-};
