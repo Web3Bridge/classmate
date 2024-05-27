de93228c046820257d8f8410f00071a68d620302
signor1 <emmanuelomemgboji@gmail.com>
2024-05-28 00:04:18 +0100

    feat: added few dev deps

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css"
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google"
+import { Web3Modal } from "@/context/Web3Modal";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Web3Modal>
+          {children}
+        </Web3Modal>
+      </body>
     </html>
   );
 }
