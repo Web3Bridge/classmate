7fa7943e454155fe11b6c73d33c002ff656b6575
JoshdfG <agboolajoshua161@gmail.com>
2024-06-02 13:05:16 +0100

    chore:added submit_id route styles

--- frontend/app/user/submit_id/page.tsx
+++ frontend/app/user/submit_id/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
 export default function SubmitID() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Card className="w-[50%] mx-auto bg-color2 text-white">
+        <CardHeader>
+          <CardTitle className="capitalize">classmate + student form</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="text-white">Enter ID</CardDescription>
+          <form action="">
+            <Input />
+          </form>
+        </CardContent>
+        <div className="flex justify-end ">
+          <CardFooter className="gap-2">
+            <Button className="bg-color1 hover:bg-color1/50 transition-all ease-in-out">
+              Cancel
+            </Button>
+            <Button className="bg-color1 hover:bg-color1/50 transition-all ease-in-out">
+              Submit
+            </Button>
+          </CardFooter>
+        </div>
+      </Card>
+    </div>
+  );
 }
