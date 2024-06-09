19be7fd665fbaa53da5bf42cb73f0d5493ef7890
signor1 <emmanuelomemgboji@gmail.com>
2024-06-10 00:42:38 +0100

    feat: added date to attendence card

--- frontend/app/admin/fileupload/page.tsx
+++ frontend/app/admin/fileupload/page.tsx
@@ -1,7 +1,10 @@
+import Upload from "@/components/admin/Upload";
 
 
 export default function FileUpload() {
     return (
-        <div>page</div>
+        <main className="w-full flex flex-col overflow-x-hidden">
+            <Upload />
+        </main>
     )
 }
---
+++ frontend/components/admin/Upload.tsx
@@ -0,0 +1,16 @@
+
+
+const Upload = () => {
+    return (
+        <section className='w-full py-6 flex flex-col'>
+            <main className='w-full flex flex-col gap-7'>
+                <div className='flex flex-col'>
+                    <h1 className='uppercase text-color2 md:text-2xl font-bold text-xl'>Upload File</h1>
+                    <h4 className='text-lg tracking-wider text-color2'>Kindly upload students or mentors file</h4>
+                </div>
+            </main>
+        </section>
+    )
+}
+
+export default Upload
