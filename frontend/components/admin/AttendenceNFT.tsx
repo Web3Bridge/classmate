45e2103e0f34244952a0b04252c9a99f672fc4f7
signor1 <emmanuelomemgboji@gmail.com>
2024-07-21 19:51:32 +0100

    feat: integrated IPFS

--- frontend/components/admin/AttendenceNFT.tsx
+++ frontend/components/admin/AttendenceNFT.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import listOfNfts from "../../utils/Attendence.json";
 import { Button } from "../ui/button";
-import { FormEvent, useMemo, useState } from "react";
+import { FormEvent, useCallback, useMemo, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -19,14 +19,60 @@ import { HiOutlineViewfinderCircle } from "react-icons/hi2";
 import { toast } from "sonner";
 import useCreateAttendance from "@/hooks/AttendanceCreationHook/useCreateAttendance";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import axios from "axios";
+import { User2Icon } from "lucide-react";
+import { FiEdit } from "react-icons/fi";
+import { SlPicture } from "react-icons/sl";
+
 
 const AttendenceNFT = () => {
   const data = useMemo(() => listOfNfts, []);
-  const [isChecked, setIsChecked] = useState(false);
+
   const { isConnected, address } = useAccount();
+  const [selectedFile, setSelectedFile] = useState();
+  const [isChecked, setIsChecked] = useState(false);
   const [lectureId, setLectureId] = useState("");
   const [imageUri, setImageURI] = useState("");
   const [topic, setTopic] = useState("");
+
+  const handleSelectImage = ({ target }: { target: any }) => {
+    setSelectedFile(target.files[0]);
+  };
+
+  const getImage = useCallback(async () => {
+    if (selectedFile) {
+      try {
+        const formData = new FormData();
+        formData.append("file", selectedFile!);
+
+        const response = await axios.post(
+          "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+              pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
+            },
+          }
+        );
+
+        const fileUrl = response.data.IpfsHash;
+        const gateWayAndhash = `https://gray-quiet-egret-248.mypinata.cloud/ipfs/${fileUrl}`
+        setImageURI(gateWayAndhash);
+        console.log(gateWayAndhash);
+
+        return fileUrl;
+
+      } catch (error) {
+        console.log("Pinata API Error:", error);
+      }
+    }
+  }, [selectedFile]);
+
+  getImage()
+
+
 
   const { createAttendance, isWriting, isConfirming } = useCreateAttendance(
     lectureId,
@@ -41,15 +87,15 @@ const AttendenceNFT = () => {
         position: "top-right",
       });
     if (!lectureId)
-      return toast.error("Please enter lectureId", {
+      return toast.error("Please enter lecture Id", {
         position: "top-right",
       });
     if (!imageUri)
-      return toast.error("Please enter admin name", {
+      return toast.error("Select image and await image URI", {
         position: "top-right",
       });
     if (!topic)
-      return toast.error("Please enter image URI", {
+      return toast.error("Please enter topic", {
         position: "top-right",
       });
 
@@ -58,6 +104,7 @@ const AttendenceNFT = () => {
     setLectureId("");
     setImageURI("");
     setTopic("");
+
   };
 
   const handleToggle = () => {
@@ -194,18 +241,51 @@ const AttendenceNFT = () => {
                 </DialogDescription>
               </DialogHeader>
               <form className="w-full grid gap-4" onSubmit={handleSubmit}>
+                <div className="w-full flex flex-col items-center">
+                  <div className="w-[80px] h-[80px] border-[0.5px] border-color3/50 rounded relative ">
+                    {selectedFile ? (
+                      <Image
+                        src={URL.createObjectURL(selectedFile)}
+                        alt="profile"
+                        className="w-full h-full object-cover"
+                        width={440}
+                        height={440}
+                        priority
+                        quality={100}
+                      />
+                    ) : (
+                      <span className="relative flex justify-center items-center w-full h-full">
+                        <SlPicture className="relative text-6xl inline-flex rounded text-gray-300" />
+                      </span>
+                    )}
+                    <input
+                      type="file"
+                      accept="image/*"
+                      hidden
+                      className="hidden"
+                      id="selectFile"
+                      onChange={handleSelectImage}
+                    />
+                    <label
+                      htmlFor="selectFile"
+                      className=" absolute -right-1 p-1 rounded-full -bottom-1 cursor-pointer bg-gray-100 border-[0.5px] border-color3/50 font-Bebas tracking-wider text-color3">
+                      <FiEdit />
+                    </label>
+                  </div>
+                </div>
+
                 <div className="flex flex-col">
                   <label
-                    htmlFor="institutionName"
+                    htmlFor="lectureId"
                     className="text-color3 font-medium ml-1"
                   >
                     Lecture ID
                   </label>
                   <input
                     type="text"
-                    name="institutionName"
-                    id="institutionName"
-                    placeholder="Enter institution name"
+                    name="lectureId"
+                    id="lectureId"
+                    placeholder="Enter lecture id"
                     className="w-full caret-color1 py-3 px-4 outline-none rounded-lg border border-color1 text-sm bg-color1/5 text-color3"
                     value={lectureId}
                     onChange={(e) => setLectureId(e.target.value)}
@@ -213,16 +293,16 @@ const AttendenceNFT = () => {
                 </div>
                 <div className="flex flex-col">
                   <label
-                    htmlFor="adminName"
+                    htmlFor="topic"
                     className="text-color3 font-medium ml-1"
                   >
                     Topic
                   </label>
                   <input
                     type="text"
-                    name="adminName"
-                    id="adminName"
-                    placeholder="Enter admin name"
+                    name="topic"
+                    id="topic"
+                    placeholder="Enter topic"
                     className="w-full caret-color1 py-3 px-4 outline-none rounded-lg border border-color1 text-sm bg-color1/5 text-color3"
                     value={topic}
                     onChange={(e) => setTopic(e.target.value)}
@@ -230,19 +310,19 @@ const AttendenceNFT = () => {
                 </div>
                 <div className="flex flex-col">
                   <label
-                    htmlFor="programmeName"
+                    htmlFor="imageUri"
                     className="text-color3 font-medium ml-1"
                   >
                     Image URI
                   </label>
                   <input
                     type="text"
-                    name="programmeName"
-                    id="programmeName"
-                    placeholder="Enter programme name"
+                    name="imageUri"
+                    id="imageUri"
+                    placeholder="Select an image and await IPFS url..."
                     className="w-full caret-color1 py-3 px-4 outline-none rounded-lg border border-color1 text-sm bg-color1/5 text-color3"
                     value={imageUri}
-                    onChange={(e) => setImageURI(e.target.value)}
+                    readOnly
                   />
                 </div>
 
