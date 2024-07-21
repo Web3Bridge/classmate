155c6ef7d0211ae6a6bd5705d5f0eb99208dcb9d
signor1 <emmanuelomemgboji@gmail.com>
2024-07-21 12:47:51 +0100

    fix: updated copyright year

--- frontend/components/shared/admin/DashboardFooter.tsx
+++ frontend/components/shared/admin/DashboardFooter.tsx
@@ -1,9 +1,18 @@
+'use client'
+import { useEffect, useState } from "react"
 
 
 const DashboardFooter = () => {
+    const [year, setYear] = useState('')
+
+    useEffect(() => {
+        const year = new Date().getFullYear()
+        setYear(year.toString())
+    }, [])
+
     return (
         <footer className="w-full flex justify-center items-center py-6 bg-color2">
-            <p className="text-sm text-gray-300">©2023 Web3bridge built on Base. All rights reserved.</p>
+            <p className="text-sm text-gray-300">©{year} Web3bridge built on Base. All rights reserved.</p>
         </footer>
     )
 }
